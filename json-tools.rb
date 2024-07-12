56994422e94571048873253bdb8f9615a88e014c
EC97B0EAB79C <EC97B0EAB79C@proton.me>
2024-07-12 14:59:19 +0900

    Updated json-tools.rb
    Added json_to_csv

--- json-tools.rb
+++ json-tools.rb
@@ -1,18 +1,28 @@
 require "json"
+require "csv"
 
 class JsonTools
   def self.json_string_to_hash(json_string)
     json_string = json_string.gsub("'", '"').gsub("False", "false").gsub("True", "true").gsub("None", "null")
-    ruby_hash = JSON.parse(json_string)
-    ruby_hash
+    json_hash = JSON.parse json_string 
+    json_hash
+  end
+  
+  def self.json_to_csv(json_file_name, csv_file_name)
+    json_file = File.read json_file_name
+    json_hash = JSON.parse json_file
+    
+    CSV.open(csv_file_name, "w") do |csv|
+      headers = json_hash.first.keys
+      csv << headers
+
+      json_hash.each do |hash|
+        csv << hash.values_at(*headers)
+      end
+    end
   end
 end
 
 begin
-  test_string = ""
-  test_hash = JsonTools.json_string_to_hash test_string
-
-  test_hash.each do |hash|
-    puts hash
-  end
+  JsonTools.json_to_csv("test.json", "test.csv")
 end
