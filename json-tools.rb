e6d02712b245ba5a52c505ab9d959d3e3d91347c
EC97B0EAB79C <EC97B0EAB79C@proton.me>
2024-07-15 16:17:31 +0900

    Updated json_to_csv
    Added expand keys feature

--- json-tools.rb
+++ json-tools.rb
@@ -8,21 +8,33 @@ class JsonTools
     json_hash
   end
   
-  def self.json_to_csv(json_file_name, csv_file_name)
+  def self.json_to_csv(json_file_name, csv_file_name, expand_key = [])
     json_file = File.read json_file_name
     json_hash = JSON.parse json_file
     
     CSV.open(csv_file_name, "w") do |csv|
-      headers = json_hash.first.keys
+      headers = json_expand_key(json_hash, expand_key)
       csv << headers
 
       json_hash.each do |hash|
-        csv << hash.values_at(*headers)
+        csv << hash_expand_to_list(hash, expand_key)
       end
     end
+  end
+
+  def self.json_expand_key(json_hash, expand_key)
+    base_keys = json_hash.first.keys - expand_key
+    base_keys + expand_key.flat_map { |key| json_hash.first[key].keys }
+  end
+
+  def self.hash_expand_to_list(hash, expand_key)
+    base_keys = hash.keys - expand_key 
+    base_list = hash.values_at(*base_keys)
+
+    base_list + expand_key.flat_map { |key| hash[key].values_at(*hash[key].keys)}
   end
 end
 
 begin
-  JsonTools.json_to_csv("test.json", "test.csv")
+  JsonTools.json_to_csv("test/test.json", "test/test.csv", ["Test"])
 end
