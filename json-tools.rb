6afd4f7aa81d15c5fb84545287b0a04099905f2d
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2024-07-18 09:58:39 +0900

    Updated json_to_csv
    Added remove key feature

--- json-tools.rb
+++ json-tools.rb
@@ -8,27 +8,28 @@ class JsonTools
     json_hash
   end
   
-  def self.json_to_csv(json_file_name, csv_file_name, expand_key = [])
+  def self.json_to_csv(json_file_name, csv_file_name, expand_key = [], remove_key = [])
     json_file = File.read json_file_name
+    json_file.gsub!("NaN", '"NaN"')
     json_hash = JSON.parse json_file
     
     CSV.open(csv_file_name, "w") do |csv|
-      headers = json_expand_key(json_hash, expand_key)
+      headers = json_expand_key(json_hash, expand_key, remove_key)
       csv << headers
 
       json_hash.each do |hash|
-        csv << hash_expand_to_list(hash, expand_key)
+        csv << hash_expand_to_list(hash, expand_key, remove_key)
       end
     end
   end
 
-  def self.json_expand_key(json_hash, expand_key)
-    base_keys = json_hash.first.keys - expand_key
+  def self.json_expand_key(json_hash, expand_key, remove_key)
+    base_keys = (json_hash.first.keys - expand_key) - remove_key
     base_keys + expand_key.flat_map { |key| json_hash.first[key].keys }
   end
 
-  def self.hash_expand_to_list(hash, expand_key)
-    base_keys = hash.keys - expand_key 
+  def self.hash_expand_to_list(hash, expand_key, remove_key)
+    base_keys = (hash.keys - expand_key) - remove_key
     base_list = hash.values_at(*base_keys)
 
     base_list + expand_key.flat_map { |key| hash[key].values_at(*hash[key].keys)}
@@ -36,5 +37,4 @@ class JsonTools
 end
 
 begin
-  JsonTools.json_to_csv("test/test.json", "test/test.csv", ["Test"])
 end
