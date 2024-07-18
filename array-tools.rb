c2eb38a59df283d362dd3b839de17b58183e87e6
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2024-07-18 11:28:42 +0900

    Created New Classes
    Added ArrayTools and CalcDist

---
+++ array-tools.rb
@@ -0,0 +1,8 @@
+class ArrayTools
+  def self.median array
+    sorted = array.sort
+    len = sorted.length
+    (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+  end
+
+end
---
+++ calc-dist.rb
@@ -0,0 +1,35 @@
+require_relative "array-tools"
+
+class CalcDist
+  def initialize data_set
+    @data_set = data_set.sort
+  end
+
+  def median
+    len = @data_set.length
+    (@data_set[(len - 1) / 2] + @data_set[len / 2]) / 2.0
+  end
+  
+  def quartiles
+    q2 = median
+    mid_index = @data_set.length / 2
+    q1 = ArrayTools.median @data_set[0...mid_index]
+    q3 = ArrayTools.median @data_set[(@data_set.length.even? ? mid_index : mid_index + 1)..-1]
+    [q1, q2, q3]
+  end 
+
+  def whiskers
+    q1, q2, q3 = quartiles
+    iqr = q3 - q1
+    lower_fence = q1 - 1.5 * iqr
+    upper_fence = q3 + 1.5 * iqr
+    
+    lower_whisker = @data_set.select { |x| x >= lower_fence }.min
+    upper_whisker = @data_set.select { |x| x <= upper_fence }.max
+
+    [lower_whisker, upper_whisker]
+  end
+end
+
+begin
+end  
